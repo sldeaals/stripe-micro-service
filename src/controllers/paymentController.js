badcef21bc15ccafe990823dda1c399673d73928
sldeaals <danielariasaguero@gmail.com>
2024-03-15 12:20:28 -0400

    feat: add cancel payment and refund payment controllers

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -1,19 +1,47 @@
-const stripeService = require('../services/stripeService');
+const { createPayment, retrievePayment, cancelPayment, refundPayment } = require('../services/stripeService');
 
-exports.createPayment = async (req, res, next) => {
+const createPaymentController = async (req, res, next) => {
   try {
-    const paymentIntent = await stripeService.createPaymentIntent(req.body.amount);
+    const paymentIntent = await createPayment(req.body.amount);
     res.json({ clientSecret: paymentIntent.client_secret });
   } catch (error) {
     next(error);
   }
 };
 
-exports.retrievePayment = async (req, res, next) => {
+const retrievePaymentController = async (req, res, next) => {
   try {
-    const payment = await stripeService.retrievePayment(req.params.paymentId);
+    const payment = await retrievePayment(req.params.paymentId);
     res.json(payment);
   } catch (error) {
     next(error);
   }
+};
+
+const cancelPaymentController = async (req, res, next) => {
+  try {
+    const { paymentId } = req.params;
+    const canceledPayment = await cancelPayment(paymentId);
+    res.status(200).json(canceledPayment);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const refundPaymentController = async (req, res, next) => {
+  try {
+    const { paymentId } = req.params;
+    const { amount } = req.body;
+    const refundedPayment = await refundPayment(paymentId, amount);
+    res.status(200).json(refundedPayment);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  createPaymentController,
+  retrievePaymentController,
+  cancelPaymentController,
+  refundPaymentController,
 };
